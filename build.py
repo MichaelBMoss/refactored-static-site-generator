368b1c34c3585da4433f236c47ea58697a65e4db
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-02-17 15:00:26 -0800

    completed phase 2 - refactoring of ssg with containers

--- build.py
+++ build.py
@@ -1,25 +1,37 @@
-def main():
-# concatenates top 3 pages to create my home page
+pages = [
+{
+"filename": "content/home.html",
+"output": "docs/mike_moss_home.html",
+},
+{
+"filename": "content/bio.html",
+"output": "docs/mike_moss_bio.html",
+},
+{
+"filename": "content/contact.html",
+"output": "docs/mike_moss_contact.html",
+},
+
+]
+
+
+
+for page in pages:
     top_template = open('templates/top.html').read()
     bottom_template = open('templates/bottom.html').read()
-    content = open('content/home.html').read()
-    mike_moss_home = top_template + content + bottom_template
-    open('docs/mike_moss_home.html', 'w+').write(mike_moss_home)
+    content = open(page['filename']).read()
+    combined = top_template + content + bottom_template
+    open(page['output'], 'w+').write(combined)
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
-# concatenates top 3 pages to create my bio page
-    top_template = open('templates/top.html').read()
-    bottom_template = open('templates/bottom.html').read()
-    content = open('content/bio.html').read()
-    mike_moss_bio = top_template + content + bottom_template
-    open('docs/mike_moss_bio.html', 'w+').write(mike_moss_bio)
 
-# concatenates top 3 pages to create my contact page
-    top_template = open('templates/top.html').read()
-    bottom_template = open('templates/bottom.html').read()
-    content = open('content/contact.html').read()
-    mike_moss_contact = top_template + content + bottom_template
-    open('docs/mike_moss_contact.html', 'w+').write(mike_moss_contact)
-
-#runs the main function    
-if __name__ == "__main__":
-    main()
