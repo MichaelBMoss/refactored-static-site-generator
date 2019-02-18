5082953224d2e6adaf3f06d223e4b53865f12e5c
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-02-17 17:30:39 -0800

    end of phase 2.  Moving on to phase 3

--- build.py
+++ build.py
@@ -1,17 +1,16 @@
 pages = [
-{
-"filename": "content/index.html",
-"output": "docs/index.html",
-},
-{
-"filename": "content/bio.html",
-"output": "docs/bio.html",
-},
-{
-"filename": "content/contact.html",
-"output": "docs/contact.html",
-},
-
+    {
+    "filename": "content/index.html",
+    "output": "docs/index.html",
+    },
+    {
+    "filename": "content/bio.html",
+    "output": "docs/bio.html",
+    },
+    {
+    "filename": "content/contact.html",
+    "output": "docs/contact.html",
+    },
 ]
 
 for page in pages:
