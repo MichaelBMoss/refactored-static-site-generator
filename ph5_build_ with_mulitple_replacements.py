8faf0fc6a731c6248110cefdf34fe7e42f58ced8
Mike Moss' Ubuntu Linux <michaelbmoss@gmail.com>
2019-02-20 22:34:49 -0800

    cleaned directory

--- ph5_build_ with_mulitple_replacements.py
+++ ph5_build_ with_mulitple_replacements.py
@@ -19,17 +19,14 @@ pages = [
     },
 ]
 
-for page in pages:
-    title = page['title']
-    link = page['link']
-    template = open("templates/base.html").read()
-    content = open(page['filename']).read()
-    combined1 = template.replace("{{content_placeholder}}", content)
-    open('docs/temp.html', "w+").write(combined1) 
-    combined2 = open("docs/temp.html").read()
-    combined3 = combined2.replace("{{title_ph}}", title)
-    open('docs/temp.html', "w+").write(combined3)
-    combined4 = open("docs/temp.html").read()
-    combined5 = combined4.replace(link, "box")
-    open(page['output'], "w+").write(combined5) 
-    
+
+def main():
+    for page in pages:
+        template = open("templates/base.html").read()
+        content = open(page['filename']).read()
+        combined1 = template.replace("{{content_placeholder}}", content)
+        combined2 = combined1.replace("{{title_ph}}", page['title'])
+        combined3 = combined2.replace(page['link'], "box")
+        open(page['output'], "w+").write(combined3)
+      
+main()
